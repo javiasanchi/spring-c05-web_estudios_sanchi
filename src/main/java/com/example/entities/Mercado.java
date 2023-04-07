61e63dd75f4f8d9c830193f85e441c703291d264
javiasanchi <122261431+javiasanchi@users.noreply.github.com>
2023-04-07 12:43:27 +0200

    Spring web Entidad Mercada

---
+++ src/main/java/com/example/entities/Mercado.java
@@ -0,0 +1,22 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "mercado")
+public class Mercado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String cif;
+
+
+}
